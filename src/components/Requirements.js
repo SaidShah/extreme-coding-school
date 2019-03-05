9ea2c05804fea948586c02f2bfdad5ffc187ba96
said shah <saidmysr20@aol.com>
2019-03-04 19:11:45 -0500

    finished making the requirements page mobile friendly

--- src/components/Requirements.js
+++ src/components/Requirements.js
@@ -6,19 +6,19 @@ class Requirements extends PureComponent {
     return (
       <div className="faq-top-margin faq-width bold-text">
         <h1 className="center-text blue-text bold-text">Program Requirements</h1>
-        <p className="center-text">Although there are not many requirements, we adhere strictly to the ones we do have. There are 5 basic requirements that will enable us to help our students achieve their goals of becoming professional developers. </p>
+        <p className="center-text black-text">Although there are not many requirements, we adhere strictly to the ones we do have. There are 5 basic requirements that will enable us to help our students achieve their goals of becoming professional developers. </p>
         <ol>
-          <li>A prospective student must own or be able to obtain a Mac laptop computer before or on the first day of class. Our curriculum is built so our students can learn by actually coding solutions for real-world problems, so having a Mac computer is strictly enforced.</li>
-          <li>A prospective student must pass the first code challenge which will test their understanding of basic HTML and CSS.</li>
-          <li>A prospective student must pass the second code challenge which will test their critical thinking ability through the use of some basic algorithms using either Java, JavaScript or Ruby.</li>
-          <li>A prospective student must pass the third code challenge which will test their problem solving ability through the use of basic data structures in a either Java or JavaScript.</li>
-          <li>After the other four reqirments are met the student will be ready to start, and there will be a payment due before, or on the first day of class, which must be paid in order to attend.</li>
+          <li className="black-text">A prospective student must own or be able to obtain a Mac laptop computer before or on the first day of class. Our curriculum is built so our students can learn by actually coding solutions for real-world problems, so having a Mac computer is strictly enforced.</li>
+          <li className="black-text">A prospective student must pass the first code challenge which will test their understanding of basic HTML and CSS.</li>
+          <li className="black-text">A prospective student must pass the second code challenge which will test their critical thinking ability through the use of some basic algorithms using either Java, JavaScript or Ruby.</li>
+          <li className="black-text">A prospective student must pass the third code challenge which will test their problem solving ability through the use of basic data structures in a either Java or JavaScript.</li>
+          <li className="black-text">After the other four reqirments are met the student will be ready to start, and there will be a payment due before, or on the first day of class, which must be paid in order to attend.</li>
         </ol>
 
         <br/>
         <br/>
         <br/>
-        <p className="center-text">Due to the competitive pricing at Extreme Coding School we have a very competitive application process, so students are encouraged to apply early, and study before their code challenges.</p>
+        <p className="center-text black-text">Due to the competitive pricing at Extreme Coding School we have a very competitive application process, so students are encouraged to apply early, and study before their code challenges.</p>
         <br/>
         <br/>
         <h1 className="center-text blue-text bold-text">Prepare For The Code Challenge</h1>
