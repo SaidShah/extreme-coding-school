956671e561a3c54eab325fb482d56fc7e884cdcf
Dylan T. Downard <dyldownard@outlook.com>
2019-03-08 23:02:27 -0500

    Re-Arranged SideBar
    Comments made to me that the SideBar should be arranged in order with the contacts on the bottom, as it is more 'convention.' IMO, looks better.

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -11,20 +11,20 @@ class SideBar extends PureComponent {
 				<p className="font-size-1-2">5/01 : Stony Brook </p>
 				<p className="font-size-1-2"> 5/22 : Stony Brook </p>
 
-				<h6 className="bold-text font-size-1-3">Contact Admissions</h6>
+				<h6 className="bold-text font-size-1-3">Our Mission</h6>
 				<hr/>
-				<p className="font-size-1-2">(###)###-####</p>
-				<p className="font-size-1-2">(###)###-####</p>
+				<p className="font-size-1-2 center-text">The mission at Extreme Coding School is to educate students through practice and not just slides or books. We want to train our students to push disciplinary frontiers, confront complex issues and engineer innovative solutions for the grand challenges of our time.
+				</p>
 
 				<h6 className="bold-text font-size-1-3">Email</h6>
 				<hr/>
 				<p className="font-size-1-2"><a className="no-transition" href="mailto:admissions@extremecodingschool.com">Enrollment</a></p>
 				<p className="font-size-1-2"><a className="no-transition" href="mailto:admissions@extremecodingschool.com">Student Services</a></p>
 
-				<h6 className="bold-text font-size-1-3">Our Mission</h6>
+				<h6 className="bold-text font-size-1-3">Contact Admissions</h6>
 				<hr/>
-				<p className="font-size-1-2 left-text">The mission at Extreme Coding School is to educate students through practice and not just slides or books. We want to train our students to push disciplinary frontiers, confront complex issues and engineer innovative solutions for the grand challenges of our time.</p>
-
+				<p className="font-size-1-2">(###)###-####</p>
+				<p className="font-size-1-2">(###)###-####</p>
 			</div>
 		)
 	}
