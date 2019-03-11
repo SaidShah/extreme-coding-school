f7ac691e08a12bfed841169c25872ed664cd7dfc
Dylan T. Downard <dyldownard@outlook.com>
2019-03-10 22:46:33 -0400

    Fixed Navbar Navigation
    Navigation (the dropdown/toggle/collapse) was not working because we were attempting to use data-target which is a Jquery/Bootstrap way, when we are in React. Fixed for react.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,63 +1,75 @@
 import React, { Component } from "react";
 
+
+
 class Navbar extends Component {
-  render() {
-    return (
-      <nav className="mb-1 navbar navbar-expand-md sticky navbar-dark color-dark-blue outside-margin">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent-333"
-          aria-controls="navbarSupportedContent-333"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+ state = {
+  navCollapsed: true
+}
+
+_onToggleNav = () => {
+  this.setState({ navCollapsed: !this.state.navCollapsed })
+}
+
+render() {
+  const {navCollapsed} = this.state
+  return (
+
+    <nav className="mb-1 navbar navbar-expand-md sticky navbar-dark color-dark-blue outside-margin">
+      <button
+        className="navbar-toggler collapsed"
+        type="button"
+        onClick={this._onToggleNav}
+        style={{zIndex:"1000"}}  
+        data-toggle="collapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div
-          className="collapse navbar-collapse"
-          id="navbarSupportedContent-333"
-        >
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link no-transition-nav bold-font" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link no-transition-nav bold-font"  href="/curriculum">
-                Curriculum
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link no-transition-nav bold-font" href="/pricing">
-                Pricing
-              </a>
-            </li>
+        <span className="navbar-toggler-icon" />
+      </button>
 
-            <li className="nav-item">
-              <a className="nav-link no-transition-nav bold-font" href="/faq">
-                FAQ
-              </a>
-            </li>
+      <div
+      className={(navCollapsed ? 'collapse' : '') + ' navbar-collapse'}
+      >
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link no-transition-nav bold-font" href="/">
+          Home
+           </a>
+          </li>
+          <li className="nav-item">
+           <a className="nav-link no-transition-nav bold-font"  href="/curriculum">
+          Curriculum
+            </a>
+          </li>
+          <li className="nav-item">
+           <a className="nav-link no-transition-nav bold-font" href="/pricing">
+          Pricing
+           </a>
+          </li>
 
-            <li className="nav-item">
-              <a href="/requirements" className="nav-link no-transition-nav bold-font ">Requirements</a>
-            </li>
+          <li className="nav-item">
+            <a className="nav-link no-transition-nav bold-font" href="/faq">
+          FAQ
+            </a>
+          </li>
 
-            <li className="nav-item">
-              <a href="/apply" className="nav-link no-transition-nav bold-font shadow light-green">Apply Now</a>
-            </li>
+          <li className="nav-item">
+            <a href="/requirements" className="nav-link no-transition-nav bold-font ">Requirements</a>
+          </li>
 
-
-          </ul>
-        </div>
+          <li className="nav-item">
+            <a href="/apply" className="nav-link no-transition-nav bold-font shadow light-green">Apply Now</a>
+          </li>
 
 
-      </nav>
+        </ul>
+      </div>
+
+
+    </nav>
     );
-  }
+}
 }
 
 export default Navbar;
