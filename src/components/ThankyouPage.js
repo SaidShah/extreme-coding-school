1697a9b9b6e354cce7758797a8c823a8967028be
Dylan T. Downard <dyldownard@outlook.com>
2019-03-16 22:45:12 -0400

    Updated Thankyou and added Favicon
    Thankyou updated with proper description/text, favicon added.

--- src/components/ThankyouPage.js
+++ src/components/ThankyouPage.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 const ThankyouPage = () => (
   <div>
-    <h1>this is the thank you page after a person is finished applying</h1>
-    
-
+  	<div>
+    	<h1 style={{textAlign:"center", marginTop:"5%", marginBottom:"5%"}}>
+    		Your application has been submitted! Thank you for applying!
+    	</h1>
+    	<h2 style={{textAlign:"center", marginTop:"5%"}}>
+    		Your application will be reviewed by our team members.
+    	</h2>
+    	<h2 style={{textAlign:"center", marginBottom:"5%"}}>
+    		We will get back to you as soon as possible!
+    	</h2>
+    </div>
   </div>
 );
 
