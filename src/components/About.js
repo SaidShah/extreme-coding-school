01c23a8ef9371537260a4e1bd8a3fae7001c9d79
Dylan T. Downard <dyldownard@outlook.com>
2019-04-02 20:25:18 -0400

    All spelling and grammar fixed

--- src/components/About.js
+++ src/components/About.js
@@ -39,7 +39,7 @@ const About = () => (
         </p>
 
         <p className="about-p">
-          At that time, a large majority of college graduates owed a student loan worth more than two brand new Audi A4 ($72,000) or, in today’s market, two brand new Tesla Model 3. This amount, with compound interest, was what they would have to pay back for the rest of their lives. All while they would be trying to get an actual car of their own to go to work, have their own little apartment or house, and do exciting things like — eat.
+          At that time, a large majority of college graduates owed a student loan worth more than two brand-new Audi A4 ($72,000) or, in today’s market, two brand-new Tesla Model 3. This amount, with compound interest, was what they would have to pay back for the rest of their lives. All while they would be trying to get an actual car of their own to go to work, have their own little apartment or house, and do exciting things like — eat.
         </p>
         <p className="about-p">
           Today, as an educator in a local college, I see firsthand many students graduating without employable skills. How can they ever pay off the amount of debt they owe? How can they ever live a rewarding life? I think there is no mystery — for many of them, they simply can’t, and they never will.  They are trapped for the rest of their lives.
@@ -62,12 +62,12 @@ const About = () => (
         </p>
 
         <p className="about-p">
-          If you have a summer off from high school, come to take an immersive AP Computer Science bootcamp. We will teach you everything to pass the AP exam. Its credits can be transferred into most colleges to save you money down the road. Or you may even take our immersive, 5 days a week, hands-on bootcamp for 8 weeks. You will learn the full-stack web development technologies both front-end and back-end. Upon completion, you will have built a portfolio of projects to show your passion and make you stand out from the crowd in your college applications.
+          If you have a summer off from high school, come to take an immersive AP Computer Science boot camp. We will teach you everything to pass the AP exam. Its credits can be transferred into most colleges to save you money down the road. Or you may even take our immersive, 5 days a week, hands-on boot camp for 8 weeks. You will learn the full-stack web development technologies both front-end and back-end. Upon completion, you will have built a portfolio of projects to show your passion and make you stand out from the crowd in your college applications.
 
         </p>
 
         <p className="about-p">
-          If you’re thinking about a career change, you may consider our immersive, 15-week, bootcamp. Come talk to us. We will explain how.
+          If you’re thinking about a career change, you may consider our immersive, 15-week, boot camp. Come talk to us. We will explain how.
         </p>
 
         <p className="about-p">
@@ -96,14 +96,14 @@ const About = () => (
         a lack of training or funding, there are many hurdles one has to cross.
       </p>
       <p className="about-p">
-        First, it’s hard to get into a Computer Science program in college. I remember when I wanted to major in CS, my counselor said, “Sorry, Said, you just don’t have enough math classes to be accepted”. That was tough for me to hear, especially when I had the passion in programming. Here are some real numbers to show how hard it is. There are about a thousand students who would like to major in CS every year at Stony Brook University, there are between 100 to 200 people who actually get in and graduate with a bachelor’s degree.
+        First, it’s hard to get into a Computer Science program in college. I remember when I wanted to major in CS, my counselor said, “Sorry, Said, you just don’t have enough math classes to be accepted”. That was tough for me to hear, especially when I had the passion in programming. Here are some real numbers to show how hard it is. There are about a thousand students who would like to major in CS every year at Stony Brook University, there are between 100 and 200 people who actually get in and graduate with a bachelor’s degree.
       </p>
       <p className="about-p">
-        Second, even if you’re lucky enough to get into a CS program, it is still hard to learn the practical skills of coding. Most professors don’t code in class. Their mode of teaching consists of hours of mind-numbing powerpoint slides covering technologies already obsolete. It is hard for many people to learn like that. I, for example, tend to learn by designing algorithms, writing code, and building tangible modern web projects. I believe there are many like-minded people who learn this way.
+        Second, even if you’re lucky enough to get into a CS program, it is still hard to learn the practical skills of coding. Most professors don’t code in class. Their mode of teaching consists of hours of mind-numbing PowerPoint slides covering technologies already obsolete. It is hard for many people to learn like that. I, for example, tend to learn by designing algorithms, writing code, and building tangible modern web projects. I believe there are many like-minded people who learn this way.
       </p>
 
       <p className="about-p">
-        Third, now suppose you’ve finished your CS degree and you would like to get a job in the industry. There are still code challenges standing between you and your dream job. If you don’t code a lot, your professors never showed you how to code properly, or your college is theory-oriented and not covering cutting-edge technologies used in the industry, you will have a hard time beating those challenges. Not to mention that you need extensive github repositories, a portfolio website, and a substantive resume to land interviews in the first place. Those are things that a traditional, selective, and expensive CS program typically fails to deliver but are most important to employers.
+        Third, now suppose you’ve finished your CS degree and you would like to get a job in the industry. There are still code challenges standing between you and your dream job. If you don’t code a lot, your professors never showed you how to code properly, or your college is theory-oriented and not covering cutting-edge technologies used in the industry, you will have a hard time beating those challenges. Not to mention that you need extensive GitHub repositories, a portfolio website, and a substantive resume to land interviews in the first place. Those are things that a traditional, selective, and expensive CS program typically fails to deliver but are most important to employers.
       </p>
       <p className="about-p">
         Fourth, what if you find your calling in coding after you’ve finished college or even graduate school, and you want a career change. You’ll be out of luck with most CS programs in the country. But our school would be happy to provide you with a learning opportunity. A community of coders in our school will help you reach your career goal.
@@ -131,7 +131,7 @@ const About = () => (
       <p className="about-p">
         Back when I was young, like many kids my age, I wanted to play video games. And I did! I played them so much that
         I wanted to make one. So I went on a long journey of self discovery and eventually learned how to code a basic
-        game in Lua. Unfortunetly, that took 4 long years because there was no resources for me to learn like I needed to.
+        game in Lua. Unfortunetly, that took 4 long years because there were no resources for me to learn like I needed to.
       </p>
       <p className="about-p">
         I was a very hyper-active kid, (and am still a hyper-active person,) so for me, sitting down and forcing
@@ -155,7 +155,7 @@ const About = () => (
       <p className="about-p">
         I want to help people avoid this problem. Because of Extreme, I'm able to do so. I am
         in the position where I can help people of all ages learn about code at different
-        skill levels and make sure they get the practical side of it all. It drives me foward,
+        skill levels and make sure they get the practical side of it all. It drives me forward,
         knowing that other people don't have to go through the trouble I did to get knowledge;
         to waste time learning theory when it didn't apply or wasn't taught right.
       </p>
