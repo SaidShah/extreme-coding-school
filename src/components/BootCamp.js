6ee68c8f45a83efc9dbfea1c4bc4d7262d4e31d5
said shah <saidmysr20@aol.com>
2019-04-04 09:49:44 -0400

    changed bootcamp to 2 parts

--- src/components/BootCamp.js
+++ src/components/BootCamp.js
@@ -1,37 +1,101 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import CurriculumSections from './CurriculumSections'
+import MiniBootcampCurriculumSections from './MiniBootcampCurriculumSections'
 import curr_img from '.././images/person-coding.svg'
+import apcs from '.././images/apcs.svg'
 import {Link} from 'react-router-dom'
 
-const BootCamp = () => (
-  <div>
-
-    <div className="padding-after-jumbotron row center-text">
 
 
-      <div className="column-right-side ">
-        <h2 className="blue-text bold-text ">Bootcamp Curriculum</h2>
-        <img alt="curriculum " src={curr_img} className="curriculum-image"/>
-        <p className="left-text p-width cirr-text">Here at Extreme Coding School we want to equip you for continued success even when
-          you graduate as a full-stack developer. We not only cover the most in-demand skills for full-stack development,
-          we also provide you with a solid foundation in Data-Structures and Algorithms.
-        </p>
-        <p className="left-text p-width cirr-text">Our curriculum starts with the back-end then dives into the front-end followed
-          by computer science fundamentals like data-structures and algorithms. Most importantly we teach you how to
-          learn so you can continue growing your skills in the years to come.
-        </p>
-        <p className="left-text p-width cirr-text">We know that prospective employers care about what you can do, and not just what you say you can do, so our program is 90% hands on starting from Day 1. During your 15 weeks at Extreme Coding School you'll gain a wide set of technical skills, put them in action, and complete the program with an array of projects, and an impressive portfolio that will help you land your dream job.
-        </p>
-        <hr/>
 
 
-        <CurriculumSections />
+class BootCamp extends Component {
+
+  state={
+    bootcamp: true
+  }
+
+  showBootcamp=()=>{
+    return (<div>
+
+        <div className="padding-after-jumbotron row center-text">
+
+
+          <div className="column-right-side ">
+            <h2 className="blue-text bold-text ">Bootcamp Curriculum</h2>
+            <img alt="curriculum " src={curr_img} className="curriculum-image"/>
+            <p className="left-text p-width cirr-text">Here at Extreme Coding School we want to equip you for continued success even when
+              you graduate as a full-stack developer. We not only cover the most in-demand skills for full-stack development,
+              we also provide you with a solid foundation in Data-Structures and Algorithms.
+            </p>
+            <p className="left-text p-width cirr-text">Our curriculum starts with the back-end then dives into the front-end followed
+              by computer science fundamentals like data-structures and algorithms. Most importantly we teach you how to
+              learn so you can continue growing your skills in the years to come.
+            </p>
+            <p className="left-text p-width cirr-text">We know that prospective employers care about what you can do, and not just what you say you can do, so our program is 90% hands on starting from Day 1. During your 15 weeks at Extreme Coding School you'll gain a wide set of technical skills, put them in action, and complete the program with an array of projects, and an impressive portfolio that will help you land your dream job.
+            </p>
+            <hr/>
+
+
+            <CurriculumSections />
+          </div>
+          <Link to="/apply/BootCamp" className="nav-link no-transition-nav bold-font shadow light-green apply-now-size">Apply Now Full Stack Coding Bootcamp</Link>
+
+        </div>
+
+      </div>)
+  }
+
+  showMiniBootcamp=()=>{
+    return (<div>
+
+        <div className="padding-after-jumbotron row center-text">
+
+
+          <div className="column-right-side ">
+            <h2 className="blue-text bold-text ">Mini Bootcamp Curriculum</h2>
+            <img alt="curriculum " src={apcs} className="curriculum-image"/>
+            <p className="left-text p-width cirr-text">Here at Extreme Coding School we want to equip you for continued success even when
+              you graduate as a full-stack developer. We not only cover the most in-demand skills for full-stack development,
+              we also provide you with a solid foundation in Data-Structures and Algorithms.
+            </p>
+            <p className="left-text p-width cirr-text">Our curriculum starts with the back-end then dives into the front-end followed
+              by computer science fundamentals like data-structures and algorithms. Most importantly we teach you how to
+              learn so you can continue growing your skills in the years to come.
+            </p>
+            <p className="left-text p-width cirr-text">We know that prospective employers care about what you can do, and not just what you say you can do, so our program is 90% hands on starting from Day 1. During your 15 weeks at Extreme Coding School you'll gain a wide set of technical skills, put them in action, and complete the program with an array of projects, and an impressive portfolio that will help you land your dream job.
+            </p>
+            <hr/>
+
+
+            <MiniBootcampCurriculumSections />
+          </div>
+          <Link to="/apply/bootcampTeens" className="nav-link no-transition-nav bold-font shadow light-green apply-now-size">Apply Now Mini Bootcamp</Link>
+
+        </div>
+
+      </div>)
+  }
+  handleClick=()=>{
+    document.getElementById('bootcampButton').innerText="View Mini Bootcamp"
+    this.state.bootcamp ? document.getElementById('bootcampButton').innerText="View Full Bootcamp" : document.getElementById('bootcampButton').innerText="View Mini Bootcamp"
+    this.setState({
+      bootcamp: !this.state.bootcamp
+    })
+  }
+
+  render() {
+    return (
+      <>
+      <div>
+        <button id="bootcampButton" className="btn" onClick={this.handleClick}>View Bootcamp</button>
       </div>
-      <Link to="/apply/BootCamp" className="nav-link no-transition-nav bold-font shadow light-green apply-now-size">Apply Now Full Stack Coding Bootcamp</Link>
+        {this.state.bootcamp ? this.showBootcamp() : this.showMiniBootcamp()}
+      </>
+    );
+  }
 
-    </div>
-
-  </div>
-);
+}
 
 export default BootCamp;
---
+++ src/components/MiniBootcampCurriculumSections.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+const MiniBootcampCurriculumSections = () => (
+  <div>
+  <div className="wrapper">
+
+
+    <div className="half">
+
+      <div className="tab">
+        <input id="tab-one" type="checkbox" name="tabs"/>
+        <label htmlFor="tab-one" className="curriculum-label">Module 1: Gain an in-depth knowledge of Git, Ruby, HTML & SQL </label>
+        <label htmlFor="tab-one" className="curriculum-label-mobile">Module 1: Git, Ruby, HTML & SQL </label>
+        <hr className="curriculum-hr"/>
+        <div className="tab-content">
+        <div className="row">
+          <div className="column-left curriculum-text">
+            <h4 className="curriculum-h4">Description:</h4>
+            <p>Begin your journey by learning version control & GIT as you dive deep into object-orientation in Ruby. Create & manipulate databases as you learn how to efficiently debug applications. Master the building blocks of the web and become fluent in HTML & CSS.</p>
+
+          </div>
+            <div className="column-right curriculum-text">
+
+            <h4 className="curriculum-h4">What You Will Learn:</h4>
+            <ul>
+              <li>Git and version control through push, pull, merge, branching, and more.</li>
+              <li>Gain a thorough understanding of Ruby including repls, arrays, objects, hashes, regex, iterators, and more.</li>
+              <li>Learn how to build beautiful UIs for web apps by using HTML & CSS</li>
+              <li>Create, normalize and manipulate tables with PostgreSQL and SQLite3</li>
+            </ul>
+
+            </div>
+        </div>
+        </div>
+      </div>
+
+      <div className="tab">
+        <input id="tab-two" type="checkbox" name="tabs"/>
+        <label htmlFor="tab-two" className="curriculum-label">Module 2: Master JSON and Frameworks like Rails and Rack</label>
+        <label htmlFor="tab-two" className="curriculum-label-mobile">Module 2: JSON, Rails and Rack</label>
+          <hr className="curriculum-hr"/>
+        <div className="tab-content">
+        <div className="row">
+          <div className="column-left curriculum-text">
+            <h4 className="curriculum-h4">Description:</h4>
+            <p>By having a good foundation in Ruby as well as knowledge of how the web works, students utilize Rails to build complex functional back-ends. Create servers, manipulate data and learn all about APIs. Experience how quickly you can build incredible apps and back-ends with Rails.</p>
+
+          </div>
+            <div className="column-right curriculum-text">
+
+            <h4 className="curriculum-h4">What You Will Learn:</h4>
+            <ul>
+              <li>Learn how to draw routes and create forms</li>
+              <li>Gain a thorough understanding of concepts like Authorization and Validation</li>
+              <li>Understand design patterns and architectural patterns such as REST and MVC</li>
+              <li>Build your own HTTP server and learn how the request / response model of the web works</li>
+            </ul>
+
+            </div>
+        </div>
+        </div>
+      </div>
+
+      <div className="tab">
+        <input id="tab-three" type="checkbox" name="tabs"/>
+        <label htmlFor="tab-three" className="curriculum-label">Module 3: Learn JavaScript, the DOM, Node and JSON</label>
+        <label htmlFor="tab-three" className="curriculum-label-mobile">Module 3: JavaScript, DOM, Node and JSON</label>
+          <hr className="curriculum-hr"/>
+        <div className="tab-content">
+
+        <div className="row">
+          <div className="column-left curriculum-text">
+            <h4 className="curriculum-h4">Description:</h4>
+            <p>Learn and understand functional programming with JavaScript which is crucal for front-end devs. Dive into detail about how the browser works and what exactly the source code comprising a web page does. Learn how to communicate with other systems to enable you to do even more with yours.</p>
+
+          </div>
+            <div className="column-right curriculum-text">
+
+            <h4 className="curriculum-h4">What You Will Learn:</h4>
+            <ul>
+              <li>Learn different approaches to the object model, javaScript syntax and functional architecture</li>
+              <li>Manipulate the DOM and use one of the webs most famous frameworks, jQuery.</li>
+              <li>Manipulate webpages with event listeners and events</li>
+              <li>Build full-stack Javascript web applications from end-to-end with JavaScript and your knowledge of Rails</li>
+              <li>Build real-time web application servers with Node and Express frameworks</li>
+            </ul>
+
+            </div>
+        </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+  </div>
+
+);
+
+export default MiniBootcampCurriculumSections;
