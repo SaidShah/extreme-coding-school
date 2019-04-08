e6532c69f7b676efb3302c3e0772cce245ebe3da
KumquatFelafel <james.lerner@stonybrook.edu>
2019-04-08 13:11:39 -0400

    Modification of Mini Bootcamp description (Needs improvement)

--- src/components/BootCamp.js
+++ src/components/BootCamp.js
@@ -81,20 +81,18 @@ class BootCamp extends Component {
             Mini-Bootcamp Curriculum
           </h2>
           <img alt="curriculum " src={apcs} className="curriculum-image" style={{marginBottom:"2%"}}/>
-          <p className="bootcamp-p">Here at Extreme Coding School we want to equip you for continued success even when
-            you graduate as a full-stack developer. We not only cover the most in-demand skills for full-stack development,
-            we also provide you with a solid foundation in Data-Structures and Algorithms.
+          <p className="bootcamp-p">Here at Extreme Coding School we want to equip you for continued success even after
+            you graduate as a full-stack developer {/*(change?)*/}. Our mini bootcamp is designed with just this in mind. This
+            pared down version, covering the first three modules of our full bootcamp offers you the tools and knowledge
+            you need to get started on your coding journey. {/*(something bit more specific here?)*/}
           </p>
-          <p className="bootcamp-p">Our curriculum starts with the back-end then dives into the front-end followed
-            by computer science fundamentals like data-structures and algorithms. Most importantly we teach you how to
-            learn so you can continue growing your skills in the years to come.
+          <p className="bootcamp-p"> {/*CHANGE*/}Our curriculum starts with the back-end then dives into the front-end
+            followed by computer science fundamentals like data-structures and algorithms. Most importantly we teach you
+            how to learn so you can continue growing your skills in the years to come.
           </p>
-          <p className="bootcamp-p">We know that prospective employers care about what you can do,
-            and not just what you say you can do,
-            so our program is 90% hands on starting from Day 1. During your 15 weeks at
-            Extreme Coding School you'll gain a wide set of technical skills, put them in action,
-            and complete the program with an array of projects,
-            and an impressive portfolio that will help you land your dream job.
+          <p className="bootcamp-p">We believe in an approach that is very hands on, so be prepared to code! During your
+            12 weeks at Extreme Coding School you'll develop a wide set of technical skills that you can put into action,
+            and begin putting together a portfolio that can help you land your dream job.
           </p>
           <hr/>
 
