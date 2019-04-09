5ee6c1ae1ef423a984c2d73fa6d4934ec1fb0691
KumquatFelafel <james.lerner@stonybrook.edu>
2019-04-09 10:33:13 -0400

    Got rid of div.

--- src/components/MiniBootcampMoreInfo.js
+++ src/components/MiniBootcampMoreInfo.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const MiniBootcampMoreInfo = () => (
-    <div>
+    <>
       <h2 className="blue-text bold-text" style={{marginBottom:"2%"}}>Schedule</h2>
       <p className="bootcamp-p">This is Bootcamp schedule.</p>
       <h2 className = "blue-text bold-text" style={{marginBottom:"2%"}}>Prerequisites</h2>
       <p className="bootcamp-p">Those who wish to take this course must first pass a coding challenge.</p>
       <h2 className = "blue-text bold-text" style={{marginBottom:"2%"}}>Tuition</h2>
       <p className="bootcamp-p">$4900</p>
-    </div>
+    </>
 )
 
 export default MiniBootcampMoreInfo;
