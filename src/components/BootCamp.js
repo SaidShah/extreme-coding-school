b9272787029b5ffaa3357b52a32420121818f17d
KumquatFelafel <james.lerner@stonybrook.edu>
2019-04-08 20:11:05 -0400

    Added tuition and prerequisites for Bootcamp

--- src/components/BootCamp.js
+++ src/components/BootCamp.js
@@ -56,6 +56,10 @@ class BootCamp extends Component {
             Coding School you'll gain a wide set of technical skills, put them in action,
             and complete the program with an array of projects, and an impressive portfolio that will help you land your dream job.
           </p>
+          <h2 className = "blue-text bold-text" style={{marginBottom:"2%"}}>Prerequisites</h2>
+          <p className="bootcamp-p">Those who wish to take this course must first pass a coding challenge.</p>
+          <h2 className = "blue-text bold-text" style={{marginBottom:"2%"}}>Tuition</h2>
+          <p className="bootcamp-p">$9000</p>
           <hr/>
 
 
