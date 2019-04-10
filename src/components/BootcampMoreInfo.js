f50aa66c1784c0de1bd0730c8897c5f9209bdad8
kumquatfelafel <lernj88@gmail.com>
2019-04-10 10:10:56 -0400

    Modified appearance of middle bar for bootcamps. Needs to be "perfected"

--- src/components/BootcampMoreInfo.js
+++ src/components/BootcampMoreInfo.js
@@ -9,11 +9,12 @@ const BootcampMoreInfo = (props) => {
               <h5 className="bold-text course-padding-btm">Duration: <span className="not-bold-text"> {props.campDuration}</span> </h5>
               <h5 className="bold-text course-padding-btm">Schedule: <span className="not-bold-text"> {props.schedule}</span> </h5>
               <h5 className="bold-text course-padding-btm">Hours: <span className="not-bold-text"> {props.campHours} </span> </h5>
-              <h5 className="bold-text course-padding-btm">Tuition: <span className="not-bold-text">{props.tuition}</span></h5>
-              <h5 className="bold-text course-padding-btm">Prerequisites: <span className="not-bold-text">You must complete a coding challenge.</span></h5>
+              <h5 className="bold-text course-padding-btm">Starting: <span className="not-bold-text"> May 11, 2019</span> </h5>
+
             </div>
             <div className="course-double-ul-div course-padding-left">
-              <h5 className="bold-text course-padding-btm">Starting: <span className="not-bold-text"> May 11, 2019</span> </h5>
+              <h5 className="bold-text course-padding-btm">Prerequisites: <span className="not-bold-text">You must complete a coding challenge.</span></h5>
+              <h5 className="bold-text course-padding-btm">Tuition: <span className="not-bold-text">{props.tuition}</span></h5>
               <Link to={props.applyLink} className="nav-link no-transition-nav bold-font shadow light-green apply-now-size">
                 Apply Now {props.campName}
               </Link>
