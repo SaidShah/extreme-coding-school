99f414f3a1fe5844083e4c822eab31b450999111
kumquatfelafel <lernj88@gmail.com>
2019-04-11 15:02:38 -0400

    Pared down the tutoring page. Etc.

--- src/components/PrivateLessons.js
+++ src/components/PrivateLessons.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import tutor from '.././images/private1.jpg'
-import tutorTwo from '.././images/private2.jpg'
+//import tutorTwo from '.././images/private2.jpg'
 
 const PrivateLessons = () => (
   <div className="course-top-div">
@@ -13,7 +13,9 @@ const PrivateLessons = () => (
       </div>
       <div className="course-desc-div">
         <p className="font-size-1-4 p-desc-width">
-        <strong>We tutor in placeholder text, among a great many things!</strong> </p>
+        <strong>We tutor any of the courses taught at our school. We also may be willing to teach other topics on a case
+          by case basis. While there are no official prerequisites, if you wanted to learn about data structures,
+          for example, you should have a basic understanding of ___. To schedule a time for tutoring, please contact us at placeholder.  </strong> </p>
       </div>
     </div>
 
@@ -21,44 +23,18 @@ const PrivateLessons = () => (
 
     <div className="course-div second-course-div">
       <div className="course-double-ul-div course-padding-left">
-        <h5 className="bold-text course-padding-btm">Ages:
-          <span className="not-bold-text"> Any</span>
-        </h5>
-        <h5 className="bold-text course-padding-btm">Skill Level:
-          <span className="not-bold-text"> Beginner - Advanced</span>
-        </h5>
         <h5 className="bold-text course-padding-btm"> Price:
           <span className="not-bold-text"> $60/hour</span>
         </h5>
+
+
       </div>
       <div className="course-double-ul-div course-padding-left">
-        <h5 className="bold-text course-padding-btm">Prerequisite:
-          <span className="not-bold-text"> None</span>
-        </h5>
-        <h5 className="bold-text course-padding-btm">Tools:
-        <span className="not-bold-text"> Macbook or Linux laptop</span>
-        </h5>
-        <h5 className="bold-text course-padding-btm">Duration:
+        <h5 className="bold-text course-padding-btm">Contact:
           <span className="not-bold-text"> 60 minute increments</span>
         </h5>
       </div>
     </div>
-
-
-
-    <div className="course-div">
-      <div className="course-desc-div-two">
-        <h4 className="course-ul-title">What we Tutor</h4>
-        <p className="font-size-1-4 p-desc-width">We tutor any of the courses taught at our school. We also may be willing to teach other topics on a case by case basis</p>
-      </div>
-      <div className="course-image-div">
-        <img src={tutorTwo} alt="front end web development extreme coding school"
-        className="course-image" style={{width:"25rem", height:"20rem"}}/>
-      </div>
-    </div>
-
-
-
 
   </div>
 );
