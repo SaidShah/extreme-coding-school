3342d83bb64bccf63bfc104301ccd9ccfd96ff01
said shah <saidmysr20@aol.com>
2019-04-15 20:13:07 -0400

    made chinese mobile responsive

--- src/components/Chinese.js
+++ src/components/Chinese.js
@@ -21,20 +21,11 @@ const Chinese = () => (
     </div>
 
     {/*list hidden until mobile*/}
-    <ul className="course-list" style={{color:"white"}}>
-      <li>
-        Design and build robust, object-oriented applications
-      </li>
-      <li>
-        Organize complex data using Java collections
-      </li>
-      <li>
-        Read/write files and handle exceptions
-      </li>
-      <li>
-        Get to know Big O notation and efficicent sorting algorithms.
-      </li>
-    </ul>
+    <Slide left>
+      <p className="course-list" style={{color:"white", width:"100%", textAlign:"center", padding:"5vw"}}>
+        滚滚长江东逝水，浪花淘尽英雄。是非成败转头空，青山依旧在，几度夕阳红。白发渔樵江渚上，惯看秋月春风。一壶浊酒喜相逢，古今多少事，都付笑谈中。是非成败转头空，青山依旧在，惯看秋月春风。一壶浊酒喜相逢，古今多少事，滚滚长江东逝水，浪花淘尽英雄。 几度夕阳红。白发渔樵江渚上，都付笑谈中......
+      </p>
+    </Slide>
   </div>
 
 
