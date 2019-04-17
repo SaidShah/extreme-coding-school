f9898cf0feeab9ca6d4b2351f9f6633339794e93
kumquatfelafel <lernj88@gmail.com>
2019-04-17 14:34:08 -0400

    So far, so good. Removed first duplicate.

--- src/components/Apcs.js
+++ src/components/Apcs.js
@@ -63,19 +63,6 @@ const Apcs = () => (
               Apply Now {props.courseName}*/}
 
     <CoursesMoreInfo courseDuration = {"8 weeks"} schedule = {"Monday & Wednesday"} courseHours = {"8:30 am - 10:30 pm | 1:00 pm - 3:00 pm"} starting = {"May 06, 2019"} applyLink = {"/apply/apCSa8mw"} courseName = {"AP Computer Science A Mon & Wed"}/>
-    <div className="course-div second-course-div">
-      <div className="course-ul-div">
-        <div className="course-double-ul-div course-padding-left">
-          <h5 className="bold-text course-padding-btm">Duration: <span className="not-bold-text"> 8 weeks</span> </h5>
-          <h5 className="bold-text course-padding-btm">Schedule: <span className="not-bold-text"> Monday & Wednesday</span> </h5>
-          <h5 className="bold-text course-padding-btm">Hours: <span className="not-bold-text"> 8:30 am - 10:30 pm | 1:00 pm - 3:00 pm</span> </h5>
-        </div>
-        <div className="course-double-ul-div course-padding-left">
-        <h5 className="bold-text course-padding-btm">Starting: <span className="not-bold-text"> May 06, 2019</span> </h5>
-        <Link to="/apply/apCSa8mw" className="nav-link no-transition-nav bold-font shadow light-green apply-now-size">Apply Now AP Computer Science A Mon & Wed</Link>
-        </div>
-      </div>
-    </div>
 
     <div className="course-div second-course-div">
       <div className="course-ul-div">
