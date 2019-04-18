7a8be0742420bf4dd48c6e4919413248f51e90c1
kumquatfelafel <lernj88@gmail.com>
2019-04-17 22:06:50 -0400

    Frontend: remove first duplicate listing

--- src/components/Frontend.js
+++ src/components/Frontend.js
@@ -78,29 +78,6 @@ const Frontend = () => (
     <div className="course-div second-course-div">
       <div className="course-double-ul-div course-padding-left">
         <h5 className="bold-text course-padding-btm">Duration:
-          <span className="not-bold-text"> 4 weeks</span>
-        </h5>
-        <h5 className="bold-text course-padding-btm">Schedule:
-          <span className="not-bold-text"> Saturday & Sunday</span>
-        </h5>
-        <h5 className="bold-text course-padding-btm"> Hours:
-          <span className="not-bold-text"> 8:30 am - 4:30 pm</span>
-        </h5>
-      </div>
-      <div className="course-double-ul-div course-padding-left">
-        <h5 className="bold-text course-padding-btm">Starting:
-          <span className="not-bold-text"> May 11, 2019</span>
-        </h5>
-
-        <Link to="/apply/frontEndWebDev4" className="nav-link no-transition-nav bold-font shadow light-green apply-now-size">
-          Apply Now 4 week course
-        </Link>
-
-      </div>
-    </div>
-    <div className="course-div second-course-div">
-      <div className="course-double-ul-div course-padding-left">
-        <h5 className="bold-text course-padding-btm">Duration:
           <span className="not-bold-text"> 8 weeks</span>
         </h5>
         <h5 className="bold-text course-padding-btm">Schedule:
