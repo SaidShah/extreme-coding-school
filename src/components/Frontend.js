0d0004ddd63d217f1809e0552d9a990072b1bc0f
kumquatfelafel <lernj88@gmail.com>
2019-04-17 22:12:06 -0400

    Frontend: removed second duplicate listing

--- src/components/Frontend.js
+++ src/components/Frontend.js
@@ -84,30 +84,6 @@ const Frontend = () => (
         courseName = {"8 week course"}
     />
 
-    <div className="course-div second-course-div">
-      <div className="course-double-ul-div course-padding-left">
-        <h5 className="bold-text course-padding-btm">Duration:
-          <span className="not-bold-text"> 8 weeks</span>
-        </h5>
-        <h5 className="bold-text course-padding-btm">Schedule:
-          <span className="not-bold-text"> Tuesday & Thursday</span>
-        </h5>
-        <h5 className="bold-text course-padding-btm"> Hours:
-          <span className="not-bold-text"> 12:30 pm - 4:30 pm</span>
-        </h5>
-      </div>
-      <div className="course-double-ul-div course-padding-left">
-        <h5 className="bold-text course-padding-btm">Starting:
-          <span className="not-bold-text"> May 07, 2019</span>
-        </h5>
-
-        <Link to="/apply/frontEndWebDev8" className="nav-link no-transition-nav bold-font shadow light-green apply-now-size">
-          Apply Now 8 week course
-        </Link>
-
-      </div>
-    </div>
-
   </div>
 );
 
